c0c52ba8e7dd318c4b430e1ec2981ff5f3299502
martined <edmundodeleon@gmail.com>
2022-04-17 14:31:35 -0400

    adding assertion for labels end l2

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,16 +1,23 @@
 describe("React TodoMVC",() => {
-   beforeEach(() => {
+    const todo_item_1= 'Buy Milk'
+    const todo_item_2= 'Buy Honey'
+    const todo_item_3= 'Buy Satoshi'
+
+    beforeEach(() => {
     cy.visit("http://localhost:8888")
    });
    
     it('adds a single todo', () => {
-        cy.get(".new-todo").type("Buy milk{enter}")
+        cy.get(".new-todo").type(`${todo_item_1}{enter}`)
         cy.get(".todo-list li").should("have.length",1)
     });
 
     it("adds three todos",() => {
-        cy.get(".new-todo").type("Buy something{enter}")
-        cy.get(".new-todo").type("Buy things{enter}")
-        cy.get(".new-todo").type("Buy else{enter}")
+        cy.get(".new-todo").type(`${todo_item_1}{enter}`)
+        cy.get(".new-todo").type(`${todo_item_2}{enter}`)
+        cy.get(".new-todo").type(`${todo_item_3}{enter}`)
+        cy.get(".todo-list li").should("have.length",3)
+        cy.get(".todo-list li").eq(0).find("label").should("contain",todo_item_1)
+
     })
 })
